077b769da7778812d2a7d39382129fb24be9d8ea
keerthi <keerthipenumarthi@gmail.com>
2024-04-05 11:28:54 +0530

    added files

--- home.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import "./App.js";
-function cart() {
-  return;
-  console.log("hello");
-}
-export default home;
---
+++ src/home.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./App.css";
+function app() {
+  return console.log("hello");
+}
+export default home;
